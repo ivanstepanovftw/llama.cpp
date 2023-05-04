bd97623f287686742de4a9d1847571def77acb06
Ivan Stepanov <ivanstepanovftw@gmail.com>
2023-05-04 18:08:10 +0300

    i

--- examples/common.h
+++ examples/common.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "llama.h"
+#include "ggml.h"
 
 #include <string>
 #include <vector>
@@ -26,18 +27,18 @@ struct gpt_params {
     int32_t n_predict     = -1;  // new tokens to predict
     int32_t n_parts       = -1;   // amount of model parts (-1 = determine from model dimensions)
     int32_t n_ctx         = 512;  // context size
-    int32_t n_batch       = 512;  // batch size for prompt processing (must be >=32 to use BLAS)
-    int32_t n_keep        = 0;    // number of tokens to keep from initial prompt
+    int32_t n_batch       = ggml_cpu_has_blas() ? 512 : 8;  // batch size for prompt processing (must be >=32 to use BLAS)
+    int32_t n_keep        = 0;    // number of tokens to keep from initial prompt (-1 = keep whole prompt)
 
     // sampling parameters
     std::unordered_map<llama_token, float> logit_bias; // logit bias for specific tokens
-    int32_t top_k             = 40;    // <= 0 to use vocab size
-    float   top_p             = 0.95f; // 1.0 = disabled
+    int32_t top_k             = 0;     // <= 0 to use vocab size
+    float   top_p             = 1.00f; // 1.0 = disabled
     float   tfs_z             = 1.00f; // 1.0 = disabled
     float   typical_p         = 1.00f; // 1.0 = disabled
-    float   temp              = 0.80f; // 1.0 = disabled
-    float   repeat_penalty    = 1.10f; // 1.0 = disabled
-    int32_t repeat_last_n     = 64;    // last n tokens to penalize (0 = disable penalty, -1 = context size)
+    float   temp              = 1.00f; // 1.0 = disabled
+    float   repeat_penalty    = 1.00f; // 1.0 = disabled
+    int32_t repeat_last_n     = 0;     // last n tokens to penalize (0 = disable penalty, -1 = context size)
     float   frequency_penalty = 0.00f; // 0.0 = disabled
     float   presence_penalty  = 0.00f; // 0.0 = disabled
     int     mirostat          = 0;     // 0 = disabled, 1 = mirostat, 2 = mirostat 2.0
